03ef7714adf48a22072ee7de8cebde031ef881a0
Kyran Bryant <kb@kyrans-mbp.mynetworksettings.com>
2025-01-08 17:27:58 -0700

    Scaffolding Tables/Entities to the backend. Snapshot of Db (DbContext)

---
+++ Server/Models/Comments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskFlow.Models;
+
+public partial class Comments
+{
+    public long Id { get; set; }
+
+    public long TaskId { get; set; }
+
+    public long UserId { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Tasks Task { get; set; } = null!;
+
+    public virtual Users User { get; set; } = null!;
+}
